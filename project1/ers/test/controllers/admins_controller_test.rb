7e23a546850ffa9ce1073950849568e19afc642f
Angel <a_martinez00@outlook.com>
2023-04-13 11:09:22 -0400

    Added a bit of testing/setup

--- project1/ers/test/controllers/admins_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class AdminsControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get admins_create_url
-    assert_response :success
-  end
-end
