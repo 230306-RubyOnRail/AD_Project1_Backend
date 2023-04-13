422acd7b0c2911b16c1e571e5a6241a9ff36c066
Dustyn Bachetti <dustyn821@revature.com>
2023-04-13 17:56:34 -0600

    Fixed alot

--- project1/ers/test/controllers/reimbursements_controller_test.rb
+++ project1/ers/test/controllers/reimbursements_controller_test.rb
@@ -3,7 +3,9 @@ require "test_helper"
 class ReimbursementsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     token = testUser('admin')
-    get "/reimbursements",
+    id = testReim(1)
+    Rails.logger.info(id)
+    get reimbursements_url,
         headers: { Authorization: "Bearer #{token}" }, as: :json
     assert_response :success
   end
@@ -21,31 +23,44 @@ class ReimbursementsControllerTest < ActionDispatch::IntegrationTest
     assert_response :unauthorized
   end
 
-  # test "should delete reimbursement" do
-  #   token = testUser('admin')
-  #   id = testReim(1)
-  #   delete "/reimbursements/#{id}",
-  #          headers: { Authorization: "Bearer #{token}" }, as: :json
-  #   assert_response :ok
-  # end
+  test "should delete reimbursement" do
+    token = testUser('admin')
+    reim = reimbursements(:reim1).id
+    assert_difference 'Reimbursement.count', -1 do
+      delete "/reimbursements/#{reim}",
+           headers: { Authorization: "Bearer #{token}" }, as: :json
+      assert_response :ok
+    end
+  end
 
-  # test "should get create" do
-  #   get reimbursements_create_url
-  #   assert_response :success
-  # end
-  #
-  # test "should get show" do
-  #   get reimbursements_show_url
-  #   assert_response :success
-  # end
-  #
-  # test "should get update" do
-  #   get reimbursements_update_url
-  #   assert_response :success
-  # end
-  #
-  # test "should get delete" do
-  #   get reimbursements_delete_url
-  #   assert_response :success
+  test "should delete owned reimbursement" do
+    #token = testUser('admin')
+    user = users(:user2)
+    token = JWT.encode({ user_id: user.id }, Rails.application.credentials.jwt_token_secret, 'HS256')
+    reim = reimbursements(:reim2).id
+    assert_difference 'Reimbursement.count', -1 do
+      delete "/reimbursements/#{reim}",
+             headers: { Authorization: "Bearer #{token}" }, as: :json
+      assert_response :ok
+    end
+  end
+
+  test "should not delete unowned reimbursement" do
+    #token = testUser('admin')
+    user = users(:user2)
+    token = JWT.encode({ user_id: user.id }, Rails.application.credentials.jwt_token_secret, 'HS256')
+    reim = reimbursements(:reim1).id
+    assert_no_difference 'Reimbursement.count' do
+      delete "/reimbursements/#{reim}",
+             headers: { Authorization: "Bearer #{token}" }, as: :json
+      assert_response :unauthorized
+    end
+  end
+
+  # test "should destroy user" do
+  #   user = users(:one)
+  #   assert_difference 'User.count', -1 do
+  #     delete user_url(user)
+  #   end
   # end
 end
